c02c53eafb7163fdd40e8cbd38bc2b0a522f3a18
Enri44 <enry.gerardi@gmail.com>
2023-04-25 21:21:26 +0200

    Completato il metodo per generare codici fiscali. Mezza scritta la funzione per leggere i dati da Comuni.xml

--- src/CFManager.java
+++ src/CFManager.java
@@ -4,9 +4,11 @@ import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import java.io.FileInputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CFManager {
     private final ArrayList<Persona> input_persone = new ArrayList<Persona>();
+
     public CFManager() {
     }
 
@@ -39,11 +41,11 @@ public class CFManager {
      * @param reader è lo strumento per il parsing inizializzato per
      *               leggere dal file InputPersone.xml
      */
-    public void leggi_nomi(XMLStreamReader reader){
-        while(true){//continua finché non viene lanciata un'eccezione quando sono finiti gli eventi dell'xml
+    public void leggi_nomi(XMLStreamReader reader) {
+        while (true) {//continua finché non viene lanciata un'eccezione quando sono finiti gli eventi dell'xml
             try {
                 if (!reader.hasNext()) break;
-                if(reader.getEventType() == XMLStreamConstants.START_ELEMENT && reader.getLocalName().equals("persona")) {
+                if (reader.getEventType() == XMLStreamConstants.START_ELEMENT && reader.getLocalName().equals("persona")) {
                     reader.next();
                     reader.next();
                     reader.next();
@@ -68,10 +70,10 @@ public class CFManager {
                     reader.next();
                     reader.next();
                     int giorno = Integer.parseInt(reader.getText().substring(8));
-                    Data.Mese mese = Data.Mese.values()[Integer.parseInt(reader.getText().substring(5,7))-1];
-                    int anno = Integer.parseInt(reader.getText().substring(0,4));
+                    Data.Mese mese = Data.Mese.values()[Integer.parseInt(reader.getText().substring(5, 7)) - 1];
+                    int anno = Integer.parseInt(reader.getText().substring(0, 4));
                     Data data = new Data(giorno, mese, anno);
-                    input_persone.add(new Persona(nome,cognome,sesso,data,citta));
+                    input_persone.add(new Persona(nome, cognome, sesso, data, citta));
                 }
                 reader.next();
             } catch (XMLStreamException e) {
@@ -81,17 +83,23 @@ public class CFManager {
 
     }
 
-    ArrayList<CodiceFiscale> cf_generati = new ArrayList<>();
+    private final ArrayList<CodiceFiscale> cf_generati = new ArrayList<>();
+
+    public ArrayList<CodiceFiscale> getCf_generati() {
+        return cf_generati;
+    }
 
     /**
      * Crea un codice fiscale per ogni persona della lista ricavata dall'input
      * e aggiunge i codici generati a un ArrayList di codici
      */
-    public void genera_cf(){
-        for(Persona persona : input_persone) {
+    public void genera_cf() {
+        for (Persona persona : input_persone) {
             StringBuffer codice = new StringBuffer();
             codice.append(cognome_CF(persona.getCognome()));
             codice.append(nome_CF(persona.getNome()));
+            codice.append(data_CF(persona.getNascita(), persona.getSesso()));
+            codice.append(SiglaComuni.get(persona.getCitta()));
             cf_generati.add(new CodiceFiscale(codice.toString()));
         }
     }
@@ -99,33 +107,32 @@ public class CFManager {
     /**
      * Seleziona le tre lettere del cognome che andranno nel codice fiscale secondo
      * le linee guida
+     *
      * @param cognome il cognome della persona da cui ricavare le tre lettere
      * @return le tre lettere sotto forma di stringa
      */
-    public String cognome_CF(String cognome){
+    public String cognome_CF(String cognome) {
         int cont = 0;
         String cognome_cf = "";
-        for(int i=0; i<cognome.length(); i++){//aggiunge tutte le consonanti che trova in ordine
-            if(cont==3)break;
-            if(cognome.charAt(i) != 'A' && cognome.charAt(i) != 'E' && cognome.charAt(i) != 'I' && cognome.charAt(i) != 'O' && cognome.charAt(i) != 'U')
-            {
-                cognome_cf+=cognome.charAt(i);
+        for (int i = 0; i < cognome.length(); i++) {//aggiunge tutte le consonanti che trova in ordine
+            if (cont == 3) break;
+            if (cognome.charAt(i) != 'A' && cognome.charAt(i) != 'E' && cognome.charAt(i) != 'I' && cognome.charAt(i) != 'O' && cognome.charAt(i) != 'U') {
+                cognome_cf += cognome.charAt(i);
                 cont++;
             }
         }
-        if(cont<3){//se finiscono le consonanti aggiunge tutte le vocali che trova in ordine
-            for(int i=0; i<cognome.length(); i++){
-                if(cont==3)break;
-                if(cognome.charAt(i) == 'A' || cognome.charAt(i) == 'E' || cognome.charAt(i) == 'I' || cognome.charAt(i) == 'O' || cognome.charAt(i) == 'U')
-                {
-                    cognome_cf+=cognome.charAt(i);
+        if (cont < 3) {//se finiscono le consonanti aggiunge tutte le vocali che trova in ordine
+            for (int i = 0; i < cognome.length(); i++) {
+                if (cont == 3) break;
+                if (cognome.charAt(i) == 'A' || cognome.charAt(i) == 'E' || cognome.charAt(i) == 'I' || cognome.charAt(i) == 'O' || cognome.charAt(i) == 'U') {
+                    cognome_cf += cognome.charAt(i);
                     cont++;
                 }
             }
         }
-        if(cont<3){//se finiscono anche le vocali aggiunge delle X per completare
-            while(cont<3){
-                cognome_cf+='X';
+        if (cont < 3) {//se finiscono anche le vocali aggiunge delle X per completare
+            while (cont < 3) {
+                cognome_cf += 'X';
                 cont++;
             }
         }
@@ -135,18 +142,19 @@ public class CFManager {
     /**
      * Seleziona le tre lettere del nome che andranno nel codice fiscale secondo
      * le linee guida
+     *
      * @param nome il nome della persona da cui ricavare le tre lettere
      * @return le tre lettere sotto forma di stringa
      */
-    public String nome_CF(String nome){
+    public String nome_CF(String nome) {
         int n_consonanti = 0;
-        for(int i=0;i<nome.length();i++){//conta le consonanti del cognome visto che la scelta delle lettere sarà diversa in base a questo numero
-            if(nome.charAt(i) != 'A' && nome.charAt(i) != 'E' && nome.charAt(i) != 'I' && nome.charAt(i) != 'O' && nome.charAt(i) != 'U')
+        for (int i = 0; i < nome.length(); i++) {//conta le consonanti del cognome visto che la scelta delle lettere sarà diversa in base a questo numero
+            if (nome.charAt(i) != 'A' && nome.charAt(i) != 'E' && nome.charAt(i) != 'I' && nome.charAt(i) != 'O' && nome.charAt(i) != 'U')
                 n_consonanti++;
         }
         String nome_cf = "";
         int cont = 0;
-        if(n_consonanti>=4) {
+        if (n_consonanti >= 4) {
             for (int i = 0; i < nome.length(); i++) {
                 if (cont == 3) break;
                 if (nome.charAt(i) != 'A' && nome.charAt(i) != 'E' && nome.charAt(i) != 'I' && nome.charAt(i) != 'O' && nome.charAt(i) != 'U') {
@@ -156,32 +164,103 @@ public class CFManager {
                     }
                 }
             }
-        }
-        else{
+        } else {
             for (int i = 0; i < nome.length(); i++) {//aggiunge tutte le consonanti che trova in ordine
                 if (cont == 3) break;
                 if (nome.charAt(i) != 'A' && nome.charAt(i) != 'E' && nome.charAt(i) != 'I' && nome.charAt(i) != 'O' && nome.charAt(i) != 'U') {
-                        nome_cf += nome.charAt(i);
-                        cont++;
-                }
-            }
-        }
-        if(cont<3){
-            for(int i=0; i<nome.length(); i++){//finite le consonanti aggiunge tutte le vocali che trova in ordine
-                if(cont==3)break;
-                if(nome.charAt(i) == 'A' || nome.charAt(i) == 'E' || nome.charAt(i) == 'I' || nome.charAt(i) == 'O' || nome.charAt(i) == 'U')
-                {
-                    nome_cf+=nome.charAt(i);
+                    nome_cf += nome.charAt(i);
                     cont++;
                 }
             }
         }
-        if(cont<3){
-            while(cont<3){//finite anche le vocali completa con delle X
-                nome_cf+='X';
+        if (cont < 3) {
+            for (int i = 0; i < nome.length(); i++) {//finite le consonanti aggiunge tutte le vocali che trova in ordine
+                if (cont == 3) break;
+                if (nome.charAt(i) == 'A' || nome.charAt(i) == 'E' || nome.charAt(i) == 'I' || nome.charAt(i) == 'O' || nome.charAt(i) == 'U') {
+                    nome_cf += nome.charAt(i);
+                    cont++;
+                }
+            }
+        }
+        if (cont < 3) {
+            while (cont < 3) {//finite anche le vocali completa con delle X
+                nome_cf += 'X';
                 cont++;
             }
         }
         return nome_cf;
     }
+    /**
+     * Seleziona le cifre del mese e del giorno e la lettera del mese
+     * per il codice fiscale secondo le linee guida
+     *
+     * @param data la data di nascita della persona
+     * @param sesso il sesso della persona
+     * @return stringa con due cifre finali dell'anno, lettera del mese, due cifre del giorno
+     */
+    public String data_CF(Data data, Persona.Sesso sesso){
+        String data_cf = "";
+        int f = 0;
+        data_cf+=(Integer.toString(data.getAnno()).substring(2));//servono solo le ultime due cifre dell'anno
+        data_cf+=(CodiceFiscale.MeseCarattere.get(data.getMese()));
+        int giorno = data.getGiorno();
+        if(sesso.equals(Persona.Sesso.Femmina))giorno+=40;//se la persona è donna al numero del giorno va aggiunto 40
+        if(giorno<10)//se la data è una sola cifra va preceduta da uno zero
+            data_cf+="0"+(giorno);
+        else data_cf+=(giorno);
+        return data_cf;
+    }
+
+
+    FileInputStream file1;
+    XMLInputFactory input1 = XMLInputFactory.newInstance();
+    XMLStreamReader reader1;
+
+    {//inizializzazione dello StreamReader per leggere il file Comuni.xml
+        try {
+            file1 = new FileInputStream("fileXML/Comuni.xml");
+            reader1 = input1.createXMLStreamReader(file1);
+            reader1.next();
+            leggi_comuni_e_sigle(reader1);
+        } catch (Exception e) {
+            System.out.println("Errore nell'inizializzazione del reader:");
+            System.out.println(e.getMessage());
+        }
+    }
+    private final ArrayList<String> comuni = new ArrayList<>();
+    private final ArrayList<String> sigle = new ArrayList<>();
+    public void leggi_comuni_e_sigle(XMLStreamReader reader1){
+        while(true){
+            try {
+                if (!reader1.hasNext()) break;
+                if(reader1.getEventType() == XMLStreamConstants.START_ELEMENT && reader1.getLocalName().equals("comune"))
+                {
+                    reader1.next();
+                    reader1.next();
+                    reader1.next();
+                    comuni.add(reader1.getText());
+                    reader1.next();
+                    reader1.next();
+                    reader1.next();
+                    reader1.next();
+                    sigle.add(reader1.getText());
+                }
+                reader1.next();
+            } catch (XMLStreamException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    /**
+     * Legge i nomi dei comuni e le sigle a loro associate dal file Comuni.xml
+     * e li associa tra loro
+     */
+    HashMap<String,String> SiglaComuni = new HashMap<String,String>()
+    {{
+        for(String c : comuni)
+            put(c,sigle.get(c.indexOf(c)));
+    }};
+
 }
+
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,9 @@ public class Main {
         Data d = new Data(5,Data.Mese.APRILE,2020);
         System.out.println(d.toString());
         CFManager c = new CFManager();
+        c.genera_cf();
         for (int i=0; i<10; i++)
-            System.out.println(c.getInput_persone().get(i));
+            System.out.println(c.getCf_generati().get(i));
     }
     
    
